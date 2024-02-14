d9ddef08ca156e21192744ef9b3cba0c03316edc
Sukrit022 <145593833+Sukrit022@users.noreply.github.com>
2024-02-14 22:22:16 +0530

    Code.go

--- Code.go
+++ Code.go
@@ -1,31 +1,50 @@
 package main
 
 import (
-	//"encoding/json"
 	"fmt"
-	//"os"
+
 	"strings"
 )
 
 func main() {
 
-	day := "Tuesday"
-	meal := "Lunch"
-	item := "Cornflakes"
-	menuItems := GetMenuItems(day, meal)
-	count := GetMenuItemscount(day, meal)
+	var day, meal, item string
+	var count int
+	var menuItems []string
+
+	fmt.Println("Enter the day:")
+	n, err := fmt.Scanln(&day)
+	if err != nil || n != 1 {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("Enter the meal:")
+	n, err = fmt.Scanln(&meal)
+	if err != nil || n != 1 {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("Enter the item(full name):")
+	n, err = fmt.Scanln(&item)
+	if err != nil || n != 1 {
+		fmt.Println("Error:", err)
+	}
+
+	menuItems = GetMenuItems(day, meal)
+	count = GetMenuItemscount(day, meal)
 
 	if menuItems != nil {
 		fmt.Println("Menu items for", strings.ToUpper(day), strings.ToUpper(meal), ": \n", menuItems)
 		fmt.Println("Number of menu items for", strings.ToUpper(day), strings.ToUpper(meal), ":", count)
-		fmt.Printf("Is %s present in the menu for the specified day and meal: %t", item, CheckMenuItems(day, meal, item))
+		fmt.Printf("Is %s present in the menu for the specified day and meal: %t \n", item, CheckMenuItems(day, meal, item))
 	}
 
-	err := ConvertMenuToJSON("Menu.xlsx")
-	if err != nil {
-		fmt.Println("Error converting menu to JSON:", err)
+	err1 := ConvertMenuToJSON("Menu.xlsx")
+	if err1 != nil {
+		fmt.Println("Error converting menu to JSON: \n", err1)
 	}
 
+	fmt.Printf("Here are the final details for the menu of %s %s : \n", strings.ToUpper(day), strings.ToUpper(meal))
 	details(day, meal)
 
 }
